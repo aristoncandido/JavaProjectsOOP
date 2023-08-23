7c266cc0f406f9686844931dafb898bb3bcee174
ariston candido <aristoncandido.nw@gmail.com>
2023-08-22 23:38:01 -0300

    construct

---
+++ Aula04/src/main/java/com/mycompany/aula04/Aula04.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.aula04;
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+   
+      
+        Caneta pen = new Caneta("Bic",0.5f,"rosa");
+       
+        
+        Caneta pen2= new Caneta("Compactor",0.7f,"azul");
+      
+        
+       
+        
+        
+    
+    }
+}
